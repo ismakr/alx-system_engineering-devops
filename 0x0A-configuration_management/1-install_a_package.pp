bef3744b1bddfc7024d93bb3a53c300e5f478633
ismark <akroutismail01@gmail.com>
2024-04-19 16:13:07 -0700

    1-2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-#install flask from pip3 Version must be 2.1.0
+# install flask from pip3 Version must be 2.1.0
+# ensure: state the package should be in
+# provider: The specific backend to use for this package
 package { 'flask':
   ensure   => '2.1.0',
   provider => pip3,
