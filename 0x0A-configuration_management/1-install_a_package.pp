3f8f795c628499c3fc1545481ef5181462eb58a5
ismark <akroutismail01@gmail.com>
2024-04-19 17:14:34 -0700

    1-

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
 # install flask v2.1.0 from pip3 Using Puppet.
-package { 'flask':
+package { ['python3-pip']:
+  ensure => installed,
+}
+package { 'pip':
+  ensure   => latest,
+  require  => Package['python3-pip'],
+  provider => 'pip',
+}
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
