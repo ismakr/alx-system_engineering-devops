486ed9c9f0eb82e552b60bcfbe3f8a315e540063
ismark <akroutismail01@gmail.com>
2024-04-19 17:34:33 -0700

    1-

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,9 @@
 # install flask v2.1.0 from pip3 Using Puppet.
-include python
-include python::virtualenv
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
