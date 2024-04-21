6fd7ba7f9f88a72f4bf5e7b5d18e65aaa10f8815
ismark <akroutismail01@gmail.com>
2024-04-21 14:45:10 -0700

    100-0

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,9 @@
 # make changes to our configuration file
 file_line { 'IdentityFile':
-  ensure => 'present',
   path   => '/etc/ssh/sshd_config',
   line   => '    IdentityFile ~/.ssh/school',
 }
 file_line { 'PasswordAuthentication':
-  ensure => 'present',
   path   => '/etc/ssh/sshd_config',
   line   => '    PasswordAuthentication no',
 }
