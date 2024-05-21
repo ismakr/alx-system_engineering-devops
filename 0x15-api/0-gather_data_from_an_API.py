9c795edda693580392579542898343484860aaa1
ismark <akroutismail01@gmail.com>
2024-05-20 17:16:25 -0700

    0-0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+for a given employee ID, returns information about his/her
+TODO list progress, using rest api
+"""
+import requests
+import sys
+import json
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    response = requests.get(url + 'todos/')
+    res = response.json()
+    k, v = 'userId', int(sys.argv[1])
+    alltask = 0
+    completed = 0
+    for user in res:
+        if k in user and v == user[k]:
+            alltask += 1
+            if 'completed' in user and user['completed'] is True:
+                completed += 1
+    response = requests.get(url + 'users/')
+    nameres = response.json()
+    k1, v1 = 'id', int(sys.argv[1])
+    for user1 in nameres:
+        if k1 in user1 and v1 == user1[k1]:
+            name = user1['name']
+    print(f"Employee {name} is done with tasks({completed}/{alltask}):")
+    for user in res:
+        if k in user and v == user[k]:
+            print(f"\t {user['title']}")
